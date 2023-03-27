02bffbc16ceb051b806f892e75aa78ac4a0c2242
kimfy <kim@wwan.no>
2023-03-27 18:27:37 +0200

    example: change rg name

--- example/terraform.tf
+++ example/terraform.tf
@@ -3,7 +3,7 @@ module "station" {
 
   environment_name                    = "dev"
   role_definition_name_on_workload_rg = "Owner"
-  station_resource_group_name         = "rg-terraform-remote-state"
+  station_resource_group_name         = "rg-terraform-station"
   federated_identity_credential_config = {
     create    = true
     audiences = ["api://AzureADTokenExchange"]
