b0543be5b33c335b0a71f43e0fcd7931665c0461
kimfy <kim@wwan.no>
2023-03-29 11:01:41 +0200

    feat: applications draft

---
+++ applications.tf
@@ -0,0 +1,17 @@
+module "caf_applications" {
+  for_each = var.applications
+  source  = "aztfmod/caf/azurerm//modules/azuread/applications_v1"
+  version = "5.7.0-preview0"
+
+  client_config = data.azurerm_client_config.current
+  user_type = each.value.user_type
+
+  // We are not using CAF
+  global_settings = {
+    passthrough = false
+  }
+
+  settings = each.value.settings
+
+  azuread_api_permissions = can(each.value.azuread_api_permissions) ? each.value.azuread_api_permissions : []
+}
--- example/terraform.tf
+++ example/terraform.tf
@@ -1,18 +1,61 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
+
 module "station" {
   source = "../"
 
   environment_name                    = "dev"
   role_definition_name_on_workload_rg = "Owner"
   station_resource_group_name         = "rg-terraform-station"
-  federated_identity_credential_config = {
-    create    = true
-    audiences = ["api://AzureADTokenExchange"]
-    issuer    = "https://token.actions.githubusercontent.com"
-    subject   = "repo:kimfy/station:environment:dev"
-  }
+  #federated_identity_credential_config = {
+  #  create    = true
+  #  audiences = ["api://AzureADTokenExchange"]
+  #  issuer    = "https://token.actions.githubusercontent.com"
+  #  subject   = "repo:kimfy/station:environment:dev"
+  #}
 
   tags = {
     "repoUrl" = "https://github.com/kimfy/station.git"
     "owner"   = "platform-engineer"
   }
+
+  applications = {
+    "backstage-azure-integration" = {
+      client_config = data.azurerm_client_config.current
+      user_type = "null"
+
+      settings = {
+        application_name = "backstage-azure-integration"
+      }
+
+      #azuread_api_permissions = [
+      #  {
+      #    resource_app_id = "00000003-0000-0000-c000-000000000000"
+      #    resource_access = [
+      #      {
+      #        id   = "98830695-27a2-44f7-8c18-0c3ebc9698f6" # GroupMember.Read.All
+      #        type = "Role"
+      #      },
+      #      {
+      #        id   = "df021288-bdef-4463-88db-98f22de89214" # User.Read.All
+      #        type = "Role"
+      #      }
+      #    ]
+      #  }
+      #]
+    }
+  }
 }
+
+data "azurerm_client_config" "current" {}
