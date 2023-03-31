b8ca28158e1fe0711f6ecd04c259b3cbbe263087
kimfy <kim@wwan.no>
2023-03-31 13:25:46 +0200

    fed cred: source display_name from variable, description (optional)

--- application_federated_identity_credential.tf
+++ application_federated_identity_credential.tf
@@ -1,8 +1,8 @@
 resource "azuread_application_federated_identity_credential" "oidc" {
   for_each              = var.federated_identity_credential_config
   application_object_id = azuread_application.workload.object_id
-  display_name          = "deployments-from-${random_id.workload.hex}-${var.environment_name}"
-  description           = "Station deployment for workload with ID ${random_id.workload.hex} in environment ${var.environment_name}"
+  display_name          = each.value.display_name
+  description           = try(each.value.description, null)
   audiences             = each.value.audiences
   issuer                = each.value.issuer
   subject               = each.value.subject
