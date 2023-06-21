06fa93aef5021451480cc00447c80c1d0346c83f
kimfy <kim@wwan.no>
2023-06-21 14:11:43 +0200

    add backend block for remote state storage

--- tests/basic.tf
+++ tests/basic.tf
@@ -1,5 +1,5 @@
 module "station-basic" {
-  source = "../"
+  source                              = "../"
   environment_name                    = "dev"
   role_definition_name_on_workload_rg = "Owner"
   station_resource_group_name         = "rg-terraform-station"
--- tests/terraform.tf
+++ tests/terraform.tf
@@ -4,6 +4,13 @@ terraform {
       source = "hashicorp/azurerm"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-station-tests"
+    storage_account_name = "sastationtests"
+    container_name       = "station-tests-remote-state"
+    key                  = "tests.tfstate"
+  }
 }
 
 provider "azurerm" {
