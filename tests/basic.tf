870b860890ceeb88dbb808a7922c78f8ef7cf551
kimfy <kim@wwan.no>
2023-06-21 17:44:59 +0200

    add resources to test module init

--- tests/basic.tf
+++ tests/basic.tf
@@ -3,4 +3,58 @@ module "station-basic" {
   environment_name                    = "dev"
   role_definition_name_on_workload_rg = "Owner"
   station_resource_group_name         = "rg-terraform-station"
+
+
+  user_assigned_identities = {
+    "station_test" = {
+      name = "uai-station-test"
+      role_assignments = [
+        "User.Read.All"
+      ]
+    }
+  }
+
+
+  # Federated Identity Credential
+  # This is attached to the workload service principal
+  federated_identity_credential_config = {
+    "apply" = {
+      display_name = "fic-station-test"
+      audiences    = ["api://AzureADTokenExchange"]
+      issuer       = "https://token.actions.githubusercontent.com"
+      subject      = "repo:kimfy/station:environment:DoesNotExist"
+    }
+  }
+
+  tags = {
+    "owner" = "DevOps kim"
+  }
+
+  groups = {
+    "dynamic" = {
+      settings = {
+        display_name     = "Station Testers"
+        owners           = [data.azurerm_client_config.current.object_id]
+        security_enabled = true
+        types            = ["DynamicMembership"]
+        dynamic_membership = {
+          "station-testers" = {
+            enabled = true
+            rule    = "user.department -eq \"Station Testers\""
+          }
+        }
+      }
+    },
+    "static" = {
+      settings = {
+        display_name     = "static"
+        owners           = [data.azurerm_client_config.current.object_id]
+        security_enabled = true
+        members          = [data.azurerm_client_config.current.object_id]
+      }
+    }
+  }
+
+  # TODO: Application - must be refactored
+
 }
