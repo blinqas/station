ed6bbf5a9ee9f56276317d308976747538f4d296
kimfy <kim@wwan.no>
2023-08-28 14:57:22 +0200

    Test file for Terraform Cloud integration

---
+++ tests/tfe/terraform_cloud.tf
@@ -0,0 +1,25 @@
+data "azurerm_client_config" "current" {}
+
+module "station-tfe" {
+  source                              = "../../"
+  environment_name                    = "test"
+
+  federated_identity_credential_config = {
+    "plan" = {
+      display_name = "plan"
+      audiences    = ["api://AzureADTokenExchange"]
+      issuer       = "https://app.terraform.io"
+      subject      = "organization:managed-devops:project:Station Development:workspace:Test:run_phase:plan"
+    },
+    "apply" = {
+      display_name = "apply"
+      audiences    = ["api://AzureADTokenExchange"]
+      issuer       = "https://app.terraform.io"
+      subject      = "organization:managed-devops:project:Station Development:workspace:Test:run_phase:apply"
+    }
+  }
+
+  tags = {
+    "owner" = "Kim Iversen"
+  }
+}
