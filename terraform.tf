c0cb3b3b006479c517ce6feddc0f0e09d92f7aeb
kimfy <kim@wwan.no>
2023-08-29 21:17:43 +0200

    Removed local provider config from Station

--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.71.0"
-    }
-
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.41.0"
-    }
-  }
-}
-
 data "azuread_client_config" "current" {}
 
 data "azurerm_client_config" "current" {}
+
