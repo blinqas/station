8f63126982b59309bbb29eb8d76d784045587831
kimfy <kim@wwan.no>
2023-08-29 10:04:30 +0200

    Fix missing argument to tfe submodule

--- tfe.tf
+++ tfe.tf
@@ -1,8 +1,9 @@
 module "station-tfe" {
   source = "./hashicorp/tfe/"
 
-  project_name   = "Station TFE Development"
-  workspace_name = "tests-tfe"
+  project_name          = "Station TFE Development"
+  workspace_name        = "tests-tfe"
+  workspace_description = "Default description"
   env_vars = {
     x = {
       value       = "x"
