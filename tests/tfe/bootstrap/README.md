d7c852e2bdf44aa872ce3e6848507d6c83dc8ffe
kimfy <kim@wwan.no>
2023-08-29 08:41:51 +0200

    Add bootstrap folder for tests/tfe

---
+++ tests/tfe/bootstrap/README.md
@@ -0,0 +1,4 @@
+# Station Deployments: Bootstrap
+
+This module deploys the application and service principal used to authenticate Terraform Cloud for Station Terraform Cloud testing.
+
---
+++ tests/tfe/bootstrap/outputs.tf
@@ -0,0 +1,24 @@
+output "client_id" {
+  value = azuread_service_principal.workload.application_id
+}
+
+output "tenant_id" {
+  value = data.azuread_client_config.current.tenant_id
+}
+
+output "subscription_id" {
+  value = data.azurerm_client_config.current.subscription_id
+}
+
+output "state_resource_group_name" {
+  value = var.state_resource_group_name 
+}
+
+output "state_storage_account_name" {
+  value = azurerm_storage_account.state.name
+}
+
+output "state_storage_container_name" {
+  value = azurerm_storage_container.state.name
+}
+
