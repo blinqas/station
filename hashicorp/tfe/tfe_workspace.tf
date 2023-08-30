1575910233e5b49ea26b9b384dd406578b91a83f
kimfy <kim@wwan.no>
2023-08-30 10:17:47 +0200

    Fixed reference to vcs_repo attributes

--- hashicorp/tfe/tfe_workspace.tf
+++ hashicorp/tfe/tfe_workspace.tf
@@ -8,10 +8,10 @@ resource "tfe_workspace" "workload" {
     content {
       identifier                 = var.vcs_repo.identifier
       branch                     = try(var.vcs_repo.branch, null)
-      ingress_submodules         = try(var.ingress_submodules.branch, false)
-      oauth_token_id             = try(var.oauth_token_id.branch, null)
-      github_app_installation_id = try(var.github_app_installation_id.branch, null)
-      tags_regex                 = try(var.tags_regex, null)
+      ingress_submodules         = try(var.vcs_repo.ingress_submodules.branch, false)
+      oauth_token_id             = try(var.vcs_repo.oauth_token_id.branch, null)
+      github_app_installation_id = try(var.vcs_repo.github_app_installation_id.branch, null)
+      tags_regex                 = try(var.vcs_repo.tags_regex, null)
     }
   }
 }
