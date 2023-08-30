e13b97b87ca931abd20dafde1927928053361f52
kimfy <kim@wwan.no>
2023-08-30 10:49:23 +0200

    Tried reconfigure the dynamic block

--- hashicorp/tfe/tfe_workspace.tf
+++ hashicorp/tfe/tfe_workspace.tf
@@ -4,15 +4,15 @@ resource "tfe_workspace" "workload" {
   project_id  = data.tfe_project.workload.id
 
   dynamic "vcs_repo" {
-    for_each = var.vcs_repo != null ? [1] : [0]
+    for_each = var.vcs_repo == null ? [] : var.vcs_repo
 
     content {
-      identifier                 = var.vcs_repo.identifier
-      branch                     = try(var.vcs_repo.branch, null)
-      ingress_submodules         = try(var.vcs_repo.ingress_submodules.branch, false)
-      oauth_token_id             = try(var.vcs_repo.oauth_token_id.branch, null)
-      github_app_installation_id = try(var.vcs_repo.github_app_installation_id.branch, null)
-      tags_regex                 = try(var.vcs_repo.tags_regex, null)
+      identifier                 = vcs_repo.value["identifier"]
+      branch                     = try(vcs_repo.value["branch"], null)
+      ingress_submodules         = try(vcs_repo.value["ingress_submodules"], false)
+      oauth_token_id             = try(vcs_repo.value["oauth_token_id"], null)
+      github_app_installation_id = try(vcs_repo.value["github_app_installation_id"], null)
+      tags_regex                 = try(vcs_repo.value["tags_regex"], null)
     }
   }
 }
