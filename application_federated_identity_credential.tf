2f28ffa4715222a70b4c77fca20b86adb01ec980
kimfy <kim@wwan.no>
2023-09-04 13:15:13 +0200

    Fixes #20. Implement option to create Federated Identity Credential in tfe block

--- application_federated_identity_credential.tf
+++ application_federated_identity_credential.tf
@@ -7,3 +7,25 @@ resource "azuread_application_federated_identity_credential" "oidc" {
   issuer                = each.value.issuer
   subject               = each.value.subject
 }
+
+locals {
+  oidc_tfe = {
+    plan = {
+      display_name = "Terraform Cloud OIDC Station Project ${random_id.workload.hex} (plan phase)"
+      subject      = "organization:${var.TFE_ORGANIZATION}:project:${var.tfe.project_name}:workspace:${var.tfe.workspace_name}:run_phase:plan"
+    },
+    apply = {
+      display_name = "Terraform Cloud OIDC Station Project ${random_id.workload.hex} (apply phase)"
+      subject      = "organization:${var.TFE_ORGANIZATION}:project:${var.tfe.project_name}:workspace:${var.tfe.workspace_name}:run_phase:apply"
+    },
+  }
+}
+
+resource "azuread_application_federated_identity_credential" "oidc-tfe" {
+  for_each              = var.tfe.create_federated_identity_credential ? [local.oidc_tfe] : [0]
+  application_object_id = azuread_application.workload.object_id
+  display_name          = each.value.display_name
+  audiences             = ["api://AzureADTokenExchange"]
+  issuer                = "https://app.terraform.io"
+  subject               = each.value.subject
+}
