4cb3ba4925f3dfee187d30753fd9a9e65df9f39a
Kim Iversen <kim@wwan.no>
2023-09-10 20:20:04 +0200

    Populate TFC workspace with variables required for Azure auth + some extras (#24)
    * Add env vars necessary to authenticate TFC Runner to Azure
    
    * Add environment_name as TFC tf var.

--- tfe.tf
+++ tfe.tf
@@ -4,12 +4,44 @@ module "station-tfe" {
   project_name          = var.tfe.project_name
   workspace_name        = var.tfe.workspace_name
   workspace_description = var.tfe.workspace_description
+  vcs_repo              = try(var.tfe.vcs_repo, null)
   env_vars = merge(try(var.tfe.env_vars, {}), {
-    station_id = {
+    # Terraform variables are prefixed with TF_VAR_ to suppress TFC Runner warning of unused variables.
+    TF_VAR_station_id = {
       value       = random_id.workload.hex
-      category    = "terraform"
+      category    = "env"
       description = "Station ID"
     },
+    TF_VAR_workload_resource_group_name = {
+      value       = azurerm_resource_group.workload.name
+      category    = "env"
+      description = "Name of the resource group created by Station"
+    },
+    TF_VAR_environment_name = {
+      value       = var.environment_name
+      category    = "env"
+      description = "Name of the current deployment environment. Often dev/test/stage/prod."
+    },
+    # DOCS: https://developer.hashicorp.com/terraform/cloud-docs/workspaces/dynamic-provider-credentials/azure-configuration#configure-terraform-cloud
+    TFC_AZURE_PROVIDER_AUTH = {
+      value       = true
+      category    = "env"
+      description = "Is true when using dynamic credentials to authenticate to Azure. https://developer.hashicorp.com/terraform/cloud-docs/workspaces/dynamic-provider-credentials/azure-configuration#configure-terraform-cloud"
+    },
+    TFC_AZURE_RUN_CLIENT_ID = {
+      value       = azuread_service_principal.workload.application_id
+      category    = "env"
+      description = "The client ID for the Service Principal / Application used when authenticating to Azure. https://developer.hashicorp.com/terraform/cloud-docs/workspaces/dynamic-provider-credentials/azure-configuration#configure-terraform-cloud"
+    },
+    ARM_SUBSCRIPTION_ID = {
+      value       = data.azurerm_client_config.current.subscription_id
+      category    = "env"
+      description = "The Subscription ID to connect to. https://developer.hashicorp.com/terraform/cloud-docs/workspaces/dynamic-provider-credentials/azure-configuration#configure-the-azurerm-or-azuread-provider"
+    },
+    ARM_TENANT_ID = {
+      value       = data.azurerm_client_config.current.tenant_id
+      category    = "env"
+      description = "The Azure Tenant ID to connect to. https://developer.hashicorp.com/terraform/cloud-docs/workspaces/dynamic-provider-credentials/azure-configuration#configure-the-azurerm-or-azuread-provider"
+    },
   })
-  vcs_repo = try(var.tfe.vcs_repo, null)
 }
