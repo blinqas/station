2f6a581e82ac66ce72672b9ca8fc1569c1329ca2
Sander Blomvågnes <110893407+Blinq-SanderBlom@users.noreply.github.com>
2023-09-11 13:41:23 +0200

    Made the bootstrap process more dynamic #25 (#29)
    * Made the bootstrap process more dynamic
    
    * Deleted the use of state storage in Azure
    
    * Deleted unsued variable
    
    ---------
    
    Co-authored-by: Sander Blomvågnes <sander@blinq.n>

--- tests/tfe/bootstrap/variables.tf
+++ tests/tfe/bootstrap/variables.tf
@@ -1,6 +1,12 @@
-variable "state_resource_group_name" {
-  type = string
-  description = "The name of the resource group to deploy State Storage Accounts in"
-  default = "rg-terraform-backend"
+variable "tfc_organization_name" {
+  type        = string
+  description = "The name of the organization you have created in Terraform Cloud"
 }
-
+variable "tfc_project_name" {
+  type        = string
+  description = "The name of the project you have created in Terraform Cloud"
+}
+variable "tfc_workspace_name" {
+  type        = string
+  description = "The name of the workspace you have created in Terraform Cloud."
+}
