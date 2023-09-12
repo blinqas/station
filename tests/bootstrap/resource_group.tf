9ce77f38fe4bbdcd3e50906d521a476f6ed1d4a9
kimfy <kim@wwan.no>
2023-09-12 21:43:08 +0200

    Remove tests folder

--- tests/bootstrap/resource_group.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "station_tests_state_storage" {
-  name     = var.state_resource_group_name
-  location = "norwayeast"
-}
--- tests/bootstrap/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "state_resource_group_name" {
-  type        = string
-  description = "The name of the resource group to deploy State Storage Accounts in"
-  default     = "rg-station-tests"
-}
-
