8064986dd259da2976fb51c4c7efb1259322c180
kimfy <kim@wwan.no>
2023-09-19 10:21:12 +0200

    Try to fix owner settings on group creation

--- groups.tf
+++ groups.tf
@@ -2,6 +2,6 @@ module "ad_groups" {
   for_each = var.groups
   source   = "./group"
   settings = each.value.settings
-  owners   = try(each.value.settings.add_workload_sp_to_owners, true) ? concat(each.value.settings.owners, [azuread_service_principal.workload.object_id]) : each.value.settings.owners
+  owners   = try(each.value.settings.add_workload_sp_to_owners, true) ? concat(try(each.value.settings.owners, []), [azuread_service_principal.workload.object_id]) : each.value.settings.owners
 }
 
