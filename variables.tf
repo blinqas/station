e7f5233e8e547564a971549a2d4182b977e4ff16
Kim Iversen <kim@wwan.no>
2023-09-19 22:16:03 +0200

    Implement Role Assignment at Station level for tenant/subscription wide permissions. (#42)
    Resolves issue #40

--- variables.tf
+++ variables.tf
@@ -89,3 +89,21 @@ variable "group_membership" {
   description = "List of group object ids the workload identity should be member of"
   default     = []
 }
+
+variable "role_assignment" {
+  type = map(object({
+    name                                   = optional(string)
+    scope                                  = string
+    role_definition_id                     = optional(string)
+    role_definition_name                   = optional(string)
+    principal_id                           = optional(string) # If null, user must set assign_to_workload_principal
+    assign_to_workload_principal           = optional(bool)
+    condition                              = optional(string)
+    condition_version                      = optional(string)
+    delegated_managed_identity_resource_id = optional(string)
+    description                            = optional(string)
+    skip_service_principal_aad_check       = optional(bool)
+  }))
+  description = "Map of azurerm_role_assignments"
+  default     = {}
+}
