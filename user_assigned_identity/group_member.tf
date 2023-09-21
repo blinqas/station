c8defb451ba7316df2fd9394ed75605571fcb0a5
Sander Blomvågnes <sander@blinq.n>
2023-09-21 10:49:23 +0200

    Tried to fix type issue

--- user_assigned_identity/group_member.tf
+++ user_assigned_identity/group_member.tf
@@ -1,6 +1,6 @@
 # Add the User Assigned Identity to each group
 resource "azuread_group_member" "uai" {
-  for_each          = toset(var.group_membership)
+  for_each          = var.group_membership
   group_object_id   = each.key
   member_object_id  = azurerm_user_assigned_identity.identity.principal_id
 }
