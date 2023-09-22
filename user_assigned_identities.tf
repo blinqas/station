3392f54d57c62290cc7d8e63739d1dd178460128
Sander Blomvågnes <sander@blinq.no>
2023-09-22 18:38:40 +0200

    Fixed if passing a null value

--- user_assigned_identities.tf
+++ user_assigned_identities.tf
@@ -2,8 +2,8 @@ module "user_assigned_identities" {
   for_each            = var.user_assigned_identities
   source              = "./user_assigned_identity/"
   name                = each.value.name
-  resource_group_name = try(each.value.resource_group_name, azurerm_resource_group.workload.name)
-  location            = try(each.value.location, azurerm_resource_group.workload.location)
+  resource_group_name = each.value.resource_group_name != null ? each.value.resource_group_name : azurerm_resource_group.workload.name
+  location            = each.value.location != null ? each.value.location : azurerm_resource_group.workload.location
   tags                = local.tags
   role_assignments    = try(each.value.role_assignments, [])
   group_membership    = try(each.value.group_membership, toset([]))
