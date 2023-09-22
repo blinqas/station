5675fae97ade37d1d0af5e2eca9ee6638a827a1d
Sander Blomvågnes <sander@blinq.no>
2023-09-22 18:42:13 +0200

    FIxed passing null

--- user_assigned_identities.tf
+++ user_assigned_identities.tf
@@ -5,6 +5,6 @@ module "user_assigned_identities" {
   resource_group_name = each.value.resource_group_name != null ? each.value.resource_group_name : azurerm_resource_group.workload.name
   location            = each.value.location != null ? each.value.location : azurerm_resource_group.workload.location
   tags                = local.tags
-  role_assignments    = try(each.value.role_assignments, [])
-  group_membership    = try(each.value.group_membership, toset([]))
+  role_assignments    = each.value.role_assignments != null ? each.value.role_assignment : []
+  group_membership    = each.value.group_membership != null ? each.value.group_membership : toset([])
 }
