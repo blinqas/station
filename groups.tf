701f289be3248e39a08f6251db068e25c273968c
kimfy <kim@wwan.no>
2023-09-25 19:04:28 +0200

    Refactor to support design decision #3

--- groups.tf
+++ groups.tf
@@ -1,11 +1,9 @@
 module "ad_groups" {
-  for_each = var.groups
-
-  source             = "./group"
-  display_name       = each.value.display_name
-  owners             = try(each.value.owners, toset([]))
-  members            = try(each.value.members, toset([]))
-  security_enabled   = each.value.security_enabled != null ? each.value.security_enabled : true
-  types              = each.value.types
-  dynamic_membership = try(each.value.dynamic_membership, [])
+  for_each      = var.groups
+  source        = "./group"
+  azuread_group = each.value
+  owners = concat(
+    each.value.owners == null ? [] : each.value.owners,
+    [azuread_service_principal.workload.object_id]
+  )
 }
