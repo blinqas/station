af08cbc5d5901f5c4ddb5bb0f447896e38cd40ac
kimfy <kim@wwan.no>
2023-09-25 20:21:52 +0200

    Added type constraints to `resource_groups`

--- resource_group.tf
+++ resource_group.tf
@@ -7,7 +7,10 @@ resource "azurerm_resource_group" "workload" {
 resource "azurerm_resource_group" "user_specified" {
   for_each = var.resource_groups
   name     = "rg-${random_id.workload.hex}-${each.key}-${var.environment_name}"
-  location = try(each.value.location, var.default_location)
-  tags     = merge(try(each.value.tags, {}), local.tags)
+  location = each.value.location == null ? var.default_location : each.value.location
+  tags = merge(
+    each.value.tags == null ? {} : each.value.tags,
+    local.tags
+  )
 }
 
