5383f67ff9d5b6f888c2bbb8d6ddfdc2e3fc4482
kimfy <kim@wwan.no>
2023-09-25 20:29:43 +0200

    Added type constraints to `federated_identity_credential_config`

--- application_federated_identity_credential.tf
+++ application_federated_identity_credential.tf
@@ -2,7 +2,7 @@ resource "azuread_application_federated_identity_credential" "oidc" {
   for_each              = var.federated_identity_credential_config
   application_object_id = azuread_application.workload.object_id
   display_name          = each.value.display_name
-  description           = try(each.value.description, null)
+  description           = each.value.description
   audiences             = each.value.audiences
   issuer                = each.value.issuer
   subject               = each.value.subject
