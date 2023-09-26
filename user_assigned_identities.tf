0813486d3982a29b70ae11553acfeed5a40e297f
Sander Blomvågnes <sander@blinq.no>
2023-09-26 14:50:11 +0200

    Updated ternary operator structure

--- user_assigned_identities.tf
+++ user_assigned_identities.tf
@@ -2,9 +2,9 @@ module "user_assigned_identities" {
   for_each            = var.user_assigned_identities
   source              = "./user_assigned_identity/"
   name                = each.value.name
-  resource_group_name = each.value.resource_group_name != null ? each.value.resource_group_name : azurerm_resource_group.workload.name
-  location            = each.value.location != null ? each.value.location : azurerm_resource_group.workload.location
+  resource_group_name = each.value.resource_group_name == null ? azurerm_resource_group.workload.name : each.value.resource_group_name
+  location            = each.value.location == null ? azurerm_resource_group.workload.location : each.value.location
   tags                = local.tags
-  role_assignments    = each.value.role_assignments != null ? each.value.role_assignment : []
-  group_membership    = each.value.group_membership != null ? each.value.group_membership : toset([])
+  role_assignments    = each.value.role_assignments == null ? [] : each.value.role_assignments
+  group_memberships   = each.value.group_memberships == null ? [] : each.value.group_memberships
 }
