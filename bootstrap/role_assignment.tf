10973033e3a623d060a41b87178348287d5f604f
Sander Blomvågnes <sander@blinq.no>
2023-09-27 22:48:23 +0200

    Added support for multiple subscriptions

--- bootstrap/role_assignment.tf
+++ bootstrap/role_assignment.tf
@@ -1,6 +1,12 @@
+data "azurerm_subscription" "deployment" {
+  for_each        = var.subscription_ids
+  subscription_id = each.key
+}
+
+
 resource "azurerm_role_assignment" "station_subscription_owner" {
-  for_each             = var.subscription_ids
-  scope                = each.value
+  for_each             = data.azurerm_subscription.deployment
+  scope                = each.value.id
   principal_id         = azuread_service_principal.workload.object_id
   role_definition_name = "Owner"
 }
@@ -13,4 +19,3 @@ resource "azuread_directory_role_assignment" "station_aad_admin" {
   role_id             = azuread_directory_role.global_admin.template_id
   principal_object_id = azuread_service_principal.workload.object_id
 }
-
