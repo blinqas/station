18257456df48dfb8925306f53e4ed351deff1b67
Sander Blomvågnes <sander@blinq.no>
2023-09-27 22:47:34 +0200

    Added all varibles as TF_VAR with comments

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-read -rp 'Terraform Cloud Organization Name: ' tfc_organization_name
-read -rp 'Name of TFC workspace to store bootstrap state: ' tfc_bootstrap_workspace_name
+# Provider authentication (`tfe`)
+export TF_CLOUD_ORGANIZATION="managed-devops" #Name of your Terraform Cloud organization 
+export TF_WORKSPACE="station-bootstrap" # Name of the station bootstrap workspace. 
+
+# Terraform variables
+export TF_VAR_tfc_organization_name=$TF_CLOUD_ORGANIZATION
+export TF_VAR_bootstrap_tfc_workspace_name=$TF_WORKSPACE
+# 
+export TF_VAR_tfc_project_name="station" #The name of the new TFC project for station
+export TF_VAR_deployments_tfc_workspace_name="station-deployments" #Workspace name for station deployments
+export TF_VAR_vcs_repo_identifier="yourOrg/station-deployments" #Name of the station deployments repo. This hsa to be created manualy
+export TF_VAR_vcs_repo_branch="trunk" #Name of the default branch for the repo you just created
+export TF_VAR_vcs_repo_github_app_installation_id="ghain-YourIdHere" #This can be found here https://app.terraform.io/api/v2/github-app/installations. This requeris that the github terraform app has allready beenn installed 
+export TF_VAR_subscription_ids="["UUID1", "UUID2"]" #Set of Azure Subscription that Station should be added as owner of. Use this command to get the subscriptions: az account list --query "[?tenantId=='yourTenantID'].{Name:name, ID:id}" --output table
+
+# Validation to ensure your not trying to bootstrap into the wrong Azure tenant 
+echo -e "Starting the bootstrap proccess against this tenant: \n \n "
 
 TENANT_ID=$(az account show --query "tenantId" -o tsv)
 
@@ -10,71 +25,28 @@ if [ -z "$TENANT_ID" ]; then
     exit 1
 fi
 
-echo Available subscriptions:; echo; echo
-az account list --query "[?tenantId=='$TENANT_ID'].{Name:name, ID:id}" --output table
-echo; echo; echo
-
-echo "Update the variables.tfvars.json file with the subscriptions_ids you want station to have access to"; echo; echo
-# Prompt the user
-read -p 'Have you updated the variables.tfvars.json file with the Azure Subscriptions? [y/n]: ' answer
-
-# Exit if the answer is 'no' or invalid
-if [[ ${answer,,} != "y" && ${answer,,} != "yes" ]]; then
-    echo "Exiting."
-    exit 1
-fi
-
-read -p 'Have you updated variables.tfvars.json with the authentication options for github. [y/n]: ' answer
-# Exit if the answer is 'no' or invalid
-if [[ ${answer,,} != "y" && ${answer,,} != "yes" ]]; then
-    echo "Exiting."
-    exit 1
-fi
-
-FILE="variables.tfvars.json" #File where we define github auth and the subscription_ids
-
-# Check if jq is installed
-if ! command -v jq &> /dev/null; then
-    echo "Error: jq is not installed. Please install jq to parse JSON."
-    exit 1
-fi
+az account show
 
 
-# Extract the values from the tfvars file
-vcs_repo_github_app_installation_id=$(jq -r '.vcs_repo_github_app_installation_id' "$FILE")
-vcs_repo_oauth_token_id=$(jq -r '.vcs_repo_oauth_token_id' "$FILE")
-subscription_ids_count=$(jq '.subscription_ids | length' "$FILE")
-#Validate that either of the vsc_ variables has beed changed to someting else than null
-if [[ "$vcs_repo_github_app_installation_id" != "null" && "$vcs_repo_oauth_token_id" != "null" ]]; then
-    echo "Error: Both vcs_repo_github_app_installation_id and vcs_repo_oauth_token_id are set. Only one of them should be set."
-    exit 1
-elif [[ "$vcs_repo_github_app_installation_id" == "null" && "$vcs_repo_oauth_token_id" == "null" ]]; then
-    echo "Error: Both vcs_repo_github_app_installation_id and vcs_repo_oauth_token_id are null. One of them should be set."
-    exit 1
-else
-    echo "The Github config has been provided"; echo; echo
-fi
+for i in {10..1}
+do
+    echo "You have $i sec to abort the script before the bootstrap start"
+    sleep 1
+done
 
-# Validate subscription_ids array is not empty
-if [ "$subscription_ids_count" -eq 0 ]; then
-    echo "Error: subscription_ids array is empty."
-    exit 1
-else 
-    echo "One or more subscription IDs has been provided"; echo; echo  
-fi
+echo "Starting the bootstrap!"
+
+
+rm ./providers.tf
 
 cp "./providers/providers_local_state.tf" "./providers.tf"
 
 terraform init -upgrade
 
-terraform workspace new "$tfc_bootstrap_workspace_name"
-
-terraform plan -var-file="variables.tfvars.json" -out "./plan.tfplan"
+terraform plan -out "./plan.tfplan"
 
 terraform apply "./plan.tfplan"
 
-# # # 
-#
 # Re-run and migrate state to Terraform Cloud
 
 rm "./providers.tf"
@@ -82,11 +54,6 @@ cp "./providers/providers.tf" "./providers.tf"
 cp "terraform.tfstate" "terraform.tfstate.before-migration.backup"
 rm "terraform.tfstate"
 
-TF_CLOUD_ORGANIZATION=$tfc_organization_name \
-TF_WORKSPACE=$tfc_bootstrap_workspace_name \
 terraform init
 
-TF_CLOUD_ORGANIZATION=$tfc_organization_name \
-TF_WORKSPACE=$tfc_bootstrap_workspace_name \
-terraform plan
-
+terraform plan -out "./plan.tfplan"
