d5bd795192cfe34c40a7cf573df4522a1663fe4c
kimfy <kim@wwan.no>
2023-10-01 19:33:21 +0200

    station-id to stationId

--- tags.tf
+++ tags.tf
@@ -1,7 +1,7 @@
 locals {
   tags = merge(
     {
-      "station-id"  = random_id.workload.hex,
+      "stationId"   = random_id.workload.hex,
       "environment" = var.environment_name,
     },
     var.tags
