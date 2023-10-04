abc710b340de5c76330f651f8e7e180285c3a0a6
Sander Blomvågnes <sander@blinq.no>
2023-10-04 12:07:35 +0200

    Added logic to add correct variable to tfc

--- bootstrap/tfe_variables.tf
+++ bootstrap/tfe_variables.tf
@@ -10,7 +10,7 @@ resource "tfe_variable" "deployments" {
 
 # These are set to enable authentication between Terraform Cloud and Azure with OIDC.
 locals {
-  env_vars = {
+  base_env_vars = {
     TFC_AZURE_PROVIDER_AUTH = {
       value       = true
       description = "Is true when using dynamic credentials to authenticate to Azure. https://developer.hashicorp.com/terraform/cloud-docs/workspaces/dynamic-provider-credentials/azure-configuration#configure-terraform-cloud"
@@ -46,30 +46,40 @@ locals {
       description = "Personal access token from Github"
       sensitive   = true
       category    = "env"
-    }
+    },
     TFE_ORGANIZATION = {
       value       = var.tfc_organization_name
       description = "Name of the Terraform Cloud organization to manage"
       sensitive   = false
       category    = "env"
-    }
+    },
     TFE_TOKEN = {
       value       = var.tfc_token
       description = "Terraform token for the 'owners' team."
       sensitive   = true
       category    = "env"
     }
-    vcs_repo_github_app_installation_id = {
-      value       = var.vcs_repo_github_app_installation_id
-      description = "ID for GitHub app installation in TFC. Ensure that the GitHub Terraform app is already installed: https://app.terraform.io/api/v2/github-app/installations."
-      sensitive   = true
-      category    = "terraform"
-    }
-    tfe_organization_name = {
-      value       = var.tfc_organization_name
-      description = "The Terraform Cloud organization to provision resources in (used by Stations station-tfe module)"
-      sensitive   = true
-      category    = "terraform"
-    }
   }
+
+  optional_env_vars = [
+    #You have to set either of them. Both can not be null 
+    var.vcs_repo_github_app_installation_id != null ? {
+      vcs_repo_github_app_installation_id = {
+        value       = var.vcs_repo_github_app_installation_id
+        description = "ID for GitHub app installation in TFC. Ensure that the GitHub Terraform app is already installed: https://app.terraform.io/api/v2/github-app/installations."
+        sensitive   = false
+        category    = "terraform"
+      }
+    } : null,
+    var.vcs_repo_oauth_token_id != null ? {
+      vcs_repo_oauth_token_id = {
+        value       = var.vcs_repo_oauth_token_id
+        description = "ID for GitHub app installation in TFC. Ensure that the GitHub Terraform app is already installed: https://app.terraform.io/api/v2/github-app/installations."
+        sensitive   = false
+        category    = "terraform"
+      }
+    } : null
+  ]
+
+  env_vars = merge(local.base_env_vars, [for env in local.optional_env_vars : env if env != null]...)
 }
