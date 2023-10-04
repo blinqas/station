94ce32862b8b28f80ca6986ebbc28fe2c253974a
kimfy <kim@wwan.no>
2023-10-04 14:15:14 +0200

    (breaking) Change var.group_membership type to be a map

--- group_membership.tf
+++ group_membership.tf
@@ -1,6 +1,6 @@
 resource "azuread_group_member" "workload" {
-  for_each         = toset(var.group_membership)
-  group_object_id  = each.key
+  for_each         = var.group_membership
+  group_object_id  = each.value
   member_object_id = module.user_assigned_identity.principal_id
 }
 
