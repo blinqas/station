8e4628ba5a2af76a349c0f6ac2baf9f8d6b7181d
kimfy <kim@wwan.no>
2023-10-26 21:28:20 +0200

    Add permissions block to role_definition

--- role_definitions.tf
+++ role_definitions.tf
@@ -5,5 +5,15 @@ resource "azurerm_role_definition" "user_created" {
   description        = each.value.description
   scope              = each.value.scope
   assignable_scopes  = each.value.assignable_scopes
+  dynamic "permissions" {
+    for_each = var.role_definitions.permissions == null ? [0] : [1]
+
+    content {
+      actions          = var.role_definitions.permissions.actions
+      not_actions      = var.role_definitions.permissions.not_actions
+      data_actions     = var.role_definitions.permissions.data_actions
+      not_data_actions = var.role_definitions.permissions.not_data_actions
+    }
+  }
 }
 
