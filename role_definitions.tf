3ce0548d52ff4666f6dc615a68cb73ca30d9eed3
Sander Blomvågnes <sander@blinq.no>
2023-11-10 11:25:20 +0100

    Fixed the permissions block for role_definitions

--- role_definitions.tf
+++ role_definitions.tf
@@ -6,14 +6,13 @@ resource "azurerm_role_definition" "user_created" {
   scope              = each.value.scope
   assignable_scopes  = each.value.assignable_scopes
   dynamic "permissions" {
-    for_each = var.role_definitions.permissions == null ? [0] : [1]
+    for_each = each.value.permissions == null ? [] : [1]
 
     content {
-      actions          = var.role_definitions.permissions.actions
-      not_actions      = var.role_definitions.permissions.not_actions
-      data_actions     = var.role_definitions.permissions.data_actions
-      not_data_actions = var.role_definitions.permissions.not_data_actions
+      actions          = each.value.permissions.actions
+      not_actions      = each.value.permissions.not_actions
+      data_actions     = each.value.permissions.data_actions
+      not_data_actions = each.value.permissions.not_data_actions
     }
   }
 }
-
