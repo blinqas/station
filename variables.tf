f90c5310bbe17b3ea4ac465944e47dc28b0a01f8
SanderBlom <sander@blomvag.no>
2024-01-02 15:15:27 +0100

    Fixed typo

--- variables.tf
+++ variables.tf
@@ -20,9 +20,9 @@ variable "resource_group_name" {
   type        = string
 }
 
-variable "managed_identiy_name" {
+variable "managed_identity_name" {
   description = <<EOF
-    The name of the managed identity (identiy provided to the workload) that is created. The final name is prefixed with `mi-`.
+    The name of the managed identity (identity provided to the workload) that is created. The final name is prefixed with `mi-`.
 
     If a value is not provided, Station will set the name to `mi-var.tfe.workspace_name-var.environment_name`
   EOF
