d0eee3d22a7432246158172bd0dd716ed5b41a10
SanderBlom <sander@blomvag.no>
2024-01-31 11:28:55 +0100

    Fix(bootstrap): Deprecated attribute in tfe_workspace

--- bootstrap/tfe_workspace.tf
+++ bootstrap/tfe_workspace.tf
@@ -3,8 +3,12 @@ resource "tfe_workspace" "bootstrap" {
   organization   = data.tfe_organization.this.name
   project_id     = tfe_project.station.id
   description    = "This workspace contains state for the bootstrap procedure for Station. Repo URL: ${var.bootstrap_repo_url}"
-  execution_mode = "local"
   force_delete   = true
+}
+
+resource "tfe_workspace_settings" "bootstrap-settings" {
+  workspace_id   = tfe_workspace.bootstrap.id
+  execution_mode = "local"
 }
 
 locals {
