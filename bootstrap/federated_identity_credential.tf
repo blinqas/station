afbd2309c63dc43371013bf98c5c9feff3586358
SanderBlom <sander@blomvag.no>
2024-01-31 11:22:37 +0100

    Fix: Deprecated application_object_id  in federated identity and azuread_application object_id reference

--- bootstrap/federated_identity_credential.tf
+++ bootstrap/federated_identity_credential.tf
@@ -1,17 +1,17 @@
 resource "azuread_application_federated_identity_credential" "tfc-plan" {
-  application_object_id = azuread_application.workload.object_id
-  display_name          = "plan"
-  description           = "Authenticate Station from Terraform Cloud to Azure"
-  audiences             = ["api://AzureADTokenExchange"]
-  issuer                = "https://app.terraform.io"
-  subject               = "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.deployments_tfc_workspace_name}:run_phase:plan"
+  application_id = azuread_application.workload.client_id
+  display_name   = "plan"
+  description    = "Authenticate Station from Terraform Cloud to Azure"
+  audiences      = ["api://AzureADTokenExchange"]
+  issuer         = "https://app.terraform.io"
+  subject        = "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.deployments_tfc_workspace_name}:run_phase:plan"
 }
 
 resource "azuread_application_federated_identity_credential" "tfc-apply" {
-  application_object_id = azuread_application.workload.object_id
-  display_name          = "apply"
-  description           = "Authenticate Station from Terraform Cloud to Azure"
-  audiences             = ["api://AzureADTokenExchange"]
-  issuer                = "https://app.terraform.io"
-  subject               = "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.deployments_tfc_workspace_name}:run_phase:apply"
+  application_id = azuread_application.workload.client_id
+  display_name   = "apply"
+  description    = "Authenticate Station from Terraform Cloud to Azure"
+  audiences      = ["api://AzureADTokenExchange"]
+  issuer         = "https://app.terraform.io"
+  subject        = "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.deployments_tfc_workspace_name}:run_phase:apply"
 }
