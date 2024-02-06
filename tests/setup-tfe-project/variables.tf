04957720c28e13f3fe06f45f01e8105877243594
Sander Blomvågnes <54621884+SanderBlom@users.noreply.github.com>
2024-02-06 15:21:01 +0100

    feat(tests): Added tests for the tfe block (#96)
    Co-authored-by: SanderBlom <sander@blomvag.no>
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Sander Blomvågnes <54621884+SanderBlom@users.noreply.github.com>

---
+++ tests/setup-tfe-project/variables.tf
@@ -0,0 +1,9 @@
+variable "tfc_organization_name" {
+  type        = string
+  description = "The name of the Terraform Cloud organization to use"
+}
+
+variable "tfc_project_name" {
+  type        = string
+  description = "The name of the Terraform Cloud project to create"
+}
