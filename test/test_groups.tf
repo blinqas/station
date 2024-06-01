ab56f4f0924bb38d0ff872874238340184490c36
Kim Iversen <kim@wwan.no>
2024-06-01 11:08:07 +0200

    test: re-add disabled tests

--- test/test_groups.tf
+++ test/test_groups.tf
@@ -1,37 +1,37 @@
-#module "station-groups" {
-#  depends_on = [tfe_project.test]
-#  source     = "./.."
-#  tfe = {
-#    project_name          = tfe_project.test.name
-#    organization_name     = data.tfe_organization.test.name
-#    workspace_description = "This workspace contains groups_tests from https://github.com/blinqas/station.git"
-#    workspace_name        = "station-tests-group_tests"
-#  }
-#  groups = {
-#    minimal = {
-#      display_name     = "Station test: groups minimal"
-#      security_enabled = true
-#    },
-#
-#    static = {
-#      display_name     = "Station test: groups static"
-#      security_enabled = true
-#      description      = "This group is static"
-#      owners           = [data.azuread_client_config.current.object_id]
-#      members          = [data.azuread_client_config.current.object_id]
-#    },
-#
-#    dynamic = {
-#      display_name     = "Station test: groups dynamic"
-#      security_enabled = true
-#      description      = "This group is dynamic"
-#      types            = ["DynamicMembership"]
-#      owners           = [data.azuread_client_config.current.object_id]
-#      dynamic_membership = {
-#        enabled = true
-#        rule    = "user.jobTitle -eq \"DevOps Engineer\""
-#      }
-#    }
-#  }
-#}
-#
+module "station-groups" {
+  depends_on = [tfe_project.test]
+  source     = "./.."
+  tfe = {
+    project_name          = tfe_project.test.name
+    organization_name     = data.tfe_organization.test.name
+    workspace_description = "This workspace contains groups_tests from https://github.com/blinqas/station.git"
+    workspace_name        = "station-tests-group_tests"
+  }
+  groups = {
+    minimal = {
+      display_name     = "Station test: groups minimal"
+      security_enabled = true
+    },
+
+    static = {
+      display_name     = "Station test: groups static"
+      security_enabled = true
+      description      = "This group is static"
+      owners           = [data.azuread_client_config.current.object_id]
+      members          = [data.azuread_client_config.current.object_id]
+    },
+
+    dynamic = {
+      display_name     = "Station test: groups dynamic"
+      security_enabled = true
+      description      = "This group is dynamic"
+      types            = ["DynamicMembership"]
+      owners           = [data.azuread_client_config.current.object_id]
+      dynamic_membership = {
+        enabled = true
+        rule    = "user.jobTitle -eq \"DevOps Engineer\""
+      }
+    }
+  }
+}
+
--- test/test_user_assigned_identities.tf
+++ test/test_user_assigned_identities.tf
@@ -1,32 +1,32 @@
-#module "station-uai" {
-#  depends_on = [tfe_project.test]
-#  source     = "./.."
-#  tfe = {
-#    project_name          = tfe_project.test.name
-#    organization_name     = data.tfe_organization.test.name
-#    workspace_description = "This workspace contains groups_tests from https://github.com/blinqas/station.git"
-#    workspace_name        = "station-tests-uai_tests"
-#  }
-#  managed_identity_name = "testName"
-#  user_assigned_identities = {
-#    minimum = {
-#      name = "uai-01"
-#    }
-#
-#    maximum = {
-#      name                 = "uai-02"
-#      location             = "norwayeast"
-#      app_role_assignments = ["User.Read.All"]
-#      group_memberships = {
-#        static = module.station-groups.groups.static.object_id
-#      }
-#      role_assignments = {
-#        subscription_reader = {
-#          role_definition_name = "Reader"
-#          scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
-#        }
-#      }
-#    }
-#  }
-#}
-#
+module "station-uai" {
+  depends_on = [tfe_project.test]
+  source     = "./.."
+  tfe = {
+    project_name          = tfe_project.test.name
+    organization_name     = data.tfe_organization.test.name
+    workspace_description = "This workspace contains groups_tests from https://github.com/blinqas/station.git"
+    workspace_name        = "station-tests-uai_tests"
+  }
+  managed_identity_name = "testName"
+  user_assigned_identities = {
+    minimum = {
+      name = "uai-01"
+    }
+
+    maximum = {
+      name                 = "uai-02"
+      location             = "norwayeast"
+      app_role_assignments = ["User.Read.All"]
+      group_memberships = {
+        static = module.station-groups.groups.static.object_id
+      }
+      role_assignments = {
+        subscription_reader = {
+          role_definition_name = "Reader"
+          scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+        }
+      }
+    }
+  }
+}
+
