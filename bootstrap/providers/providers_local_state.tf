54f90869a2f6c9c6a555d869b11a400636143069
SanderBlom <sander@blomvag.no>
2024-10-02 14:35:01 +0200

    feat: upgraded the azuread provider to support v3

--- bootstrap/providers/providers_local_state.tf
+++ bootstrap/providers/providers_local_state.tf
@@ -7,7 +7,7 @@ terraform {
 
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.36"
+      version = ">= 2.45, < 4"
     }
 
     tfe = {
--- test/providers.tf
+++ test/providers.tf
@@ -6,7 +6,7 @@ terraform {
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~>2.45"
+      version = ">= 2.45, < 4"
     }
     tfe = {
       source  = "hashicorp/tfe"
