dba6f9290c37b316c4e9f7aeb34f73921d8622c0
SanderBlom <sander@blomvag.no>
2024-11-07 12:46:24 +0100

    feat(boostrap): renamed the station azure ad application and its notes

--- bootstrap/application.tf
+++ bootstrap/application.tf
@@ -1,6 +1,6 @@
 resource "azuread_application" "workload" {
-  display_name = "app-station-bootstrap"
-  notes        = "This application tests Station TFE tests (github.com/kimfy/Station.git) from TFC to Azure"
+  display_name = "station-deployments"
+  notes        = "This application is used to create new workloads using the station module"
 }
 
 
