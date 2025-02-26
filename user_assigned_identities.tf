e89ecbb535989ef28444385a7334bb9c8c6ce9e4
Kim Iversen <kim@wwan.no>
2025-02-26 10:18:45 +0100

    style: fmt

--- user_assigned_identities.tf
+++ user_assigned_identities.tf
@@ -33,10 +33,10 @@ module "user_assigned_identity" {
   source                     = "./user_assigned_identity"
   resource_group_name        = azurerm_resource_group.workload.name
   location                   = azurerm_resource_group.workload.location
-  tags                       = local.tags
   app_role_assignments       = var.identity.app_role_assignments
-  group_memberships          = var.identity.group_memberships
   directory_role_assignments = var.identity.directory_role_assignments
+  group_memberships          = var.identity.group_memberships
+  tags                       = local.tags
 }
 
 moved {
@@ -53,14 +53,14 @@ resource "azuread_app_role_assignment" "this" {
 
 module "user_assigned_identities" {
   for_each                   = var.user_assigned_identities
-  source                     = "./user_assigned_identity/"
+  source                     = "./user_assigned_identity"
   name                       = each.value.name
   resource_group_name        = each.value.resource_group_name == null ? azurerm_resource_group.workload.name : each.value.resource_group_name
   location                   = each.value.location == null ? azurerm_resource_group.workload.location : each.value.location
-  tags                       = local.tags
   role_assignments           = each.value.role_assignments
   app_role_assignments       = each.value.app_role_assignments
-  group_memberships          = each.value.group_memberships
   directory_role_assignments = each.value.directory_role_assignments
+  group_memberships          = each.value.group_memberships
+  tags                       = local.tags
 }
 
