5f43b93485b8c9265ffb7ab12db3ed74ebead541
SanderBlom <sander@blomvag.no>
2025-03-03 10:00:35 +0100

    fix: changed to use data sources to get the MS SPs

--- tests/setup-application/main.tf
+++ tests/setup-application/main.tf
@@ -21,22 +21,21 @@ output "uuid_max" {
 
 data "azuread_application_published_app_ids" "well_known" {}
 
-resource "azuread_service_principal" "MicrosoftGraph" {
-  client_id    = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
-  use_existing = true
+
+data "azuread_service_principal" "MicrosoftGraph" {
+  client_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
 }
 
-resource "azuread_service_principal" "Office365ExchangeOnline" {
-  client_id    = data.azuread_application_published_app_ids.well_known.result.Office365ExchangeOnline
-  use_existing = true
+data "azuread_service_principal" "Office365ExchangeOnline" {
+  client_id = data.azuread_application_published_app_ids.well_known.result.Office365ExchangeOnline
 }
 
 output "MicrosoftGraph" {
-  value = azuread_service_principal.MicrosoftGraph
+  value = data.azuread_service_principal.MicrosoftGraph
 }
 
 output "Office365ExchangeOnline" {
 
-  value = azuread_service_principal.Office365ExchangeOnline
+  value = data.azuread_service_principal.Office365ExchangeOnline
 }
 
