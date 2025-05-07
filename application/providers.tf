e14663f4123f82e4ad06427b23429e499a25630c
Kim Iversen <kim@wwan.no>
2025-05-07 09:13:21 +0200

    chore: provider requirements for all submodules (#201)

---
+++ application/providers.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = "~> 1.6"
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~>2.45"
+    }
+  }
+}
+
---
+++ hashicorp/tfe/providers.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "~>0.65"
+    }
+  }
+}
+
